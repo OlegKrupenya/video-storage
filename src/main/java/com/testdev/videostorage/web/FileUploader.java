3e5ca35e834e1e7e636633ec0d5b2a32638820ae
Oleh Krupenia <oleh.krupenia@globallogic.com>
2016-06-02 16:13:49 +0300

    adding of the servlet

--- src/main/java/com/testdev/videostorage/web/FileUploader.java
+++ src/main/java/com/testdev/videostorage/web/FileUploader.java
@@ -1,7 +1,30 @@
 package com.testdev.videostorage.web;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * @author oleh.krupenia.
  */
-public class FileUploader {
+@WebServlet("/Simple")
+public class FileUploader extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+
+        out.print("<html><body>");
+        out.print("<h3>Hello Servlet</h3>");
+        out.print("</body></html>");
+    }
 }
+
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -7,10 +7,17 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>File Uploading Form</title>
+</head>
+<body>
+    <h3>File Upload:</h3>
+    Select a file to upload: <br/>
+    <form action="UploadServlet" method="post"
+          enctype="multipart/form-data">
+        <input type="file" name="file" size="50"/>
+        <br/>
+        <input type="submit" value="Upload File"/>
+    </form>
+</body>
 </html>
