0b1bdd622c45f11d4f048ed644c847edac43c98e
Oleh Krupenia <oleh.krupenia@globallogic.com>
2016-06-02 15:49:54 +0300

    creation of the domain objects

---
+++ src/main/java/com/testdev/videostorage/domain/Video.java
@@ -0,0 +1,43 @@
+package com.testdev.videostorage.domain;
+
+/**
+ * @author oleh.krupenia.
+ */
+public class Video {
+    private Long videoId;
+    private String title;
+    private byte[] content;
+    private Long userId;
+
+    public Long getVideoId() {
+        return videoId;
+    }
+
+    public void setVideoId(Long videoId) {
+        this.videoId = videoId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
