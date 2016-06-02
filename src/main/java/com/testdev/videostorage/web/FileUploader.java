ed6b792bd0b8033ca54ed349596281d2b918eee6
Oleh Krupenia <oleh.krupenia@globallogic.com>
2016-06-02 16:34:47 +0300

    uploading of the file

--- src/main/java/com/testdev/videostorage/web/FileUploader.java
+++ src/main/java/com/testdev/videostorage/web/FileUploader.java
@@ -1,19 +1,26 @@
 package com.testdev.videostorage.web;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.http.Part;
+import java.io.*;
 
 /**
  * @author oleh.krupenia.
  */
-@WebServlet("/Simple")
+@WebServlet("/upload")
+@MultipartConfig
 public class FileUploader extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    /**
+     * Name of the directory where uploaded files will be saved, relative to
+     * the web application directory.
+     */
+    private static final String SAVE_DIR = "uploadFiles";
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -25,6 +32,47 @@ public class FileUploader extends HttpServlet {
         out.print("<html><body>");
         out.print("<h3>Hello Servlet</h3>");
         out.print("</body></html>");
+    }
+
+    /**
+     * handles file upload
+     */
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+        // gets absolute path of the web application
+        String appPath = request.getServletContext().getRealPath("");
+        // constructs path of the directory to save uploaded file
+        String savePath = appPath + File.separator + SAVE_DIR;
+
+        // creates the save directory if it does not exists
+        File fileSaveDir = new File(savePath);
+        if (!fileSaveDir.exists()) {
+            fileSaveDir.mkdir();
+        }
+
+        for (Part part : request.getParts()) {
+            String fileName = extractFileName(part);
+            part.write(savePath + File.separator + fileName);
+        }
+
+        request.setAttribute("message", "Upload has been done successfully!");
+        response.getWriter().print("<h3>Upload has been done successfully!</h3>");
+//        getServletContext().getRequestDispatcher("/message.jsp").forward(
+//                request, response);
+    }
+
+    /**
+     * Extracts file name from HTTP header content-disposition
+     */
+    private String extractFileName(Part part) {
+        String contentDisp = part.getHeader("content-disposition");
+        String[] items = contentDisp.split(";");
+        for (String s : items) {
+            if (s.trim().startsWith("filename")) {
+                return s.substring(s.indexOf("=") + 2, s.length()-1);
+            }
+        }
+        return "";
     }
 }
 
