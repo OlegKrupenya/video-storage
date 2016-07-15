e234df7f20b8bc2a466074ad9c3fd04ce2c43c6a
Oleh Krupenia <oleh.krupenia@globallogic.com>
2016-07-16 00:58:53 +0300

    filter is working

--- src/main/java/com/testdev/videostorage/web/SecurityFilter.java
+++ src/main/java/com/testdev/videostorage/web/SecurityFilter.java
@@ -2,12 +2,14 @@ package com.testdev.videostorage.web;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
  * @author oleh.krupenia.
  */
-@WebFilter(filterName="securityFilter", urlPatterns = {"/*"})
+@WebFilter(filterName = "securityFilter", urlPatterns = {"/*"})
 public class SecurityFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -17,8 +19,13 @@ public class SecurityFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         System.out.println("Inside filter.");
-        // TODO: if there is no user in the session, redirect to the login page
-        chain.doFilter(request, response);
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        if (httpServletRequest.getSession(false) != null && httpServletRequest.getSession().getAttribute("user") != null
+                || httpServletRequest.getRequestURI().contains("login")) {
+            chain.doFilter(request, response);
+        } else {
+            ((HttpServletResponse) response).sendRedirect("/login.html");
+        }
     }
 
     @Override
